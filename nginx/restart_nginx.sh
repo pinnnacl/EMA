6a0caecfc51ed9cd8174761fda8466e4fdd3cbfe
Sachin Chauhan <119149964+git-hub-sachin@users.noreply.github.com>
2024-12-12 17:12:12 +0530

    Delete nginx directory

--- nginx/restart_nginx.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo rsync -avh /home/deployer/ema/nginx/ /etc/nginx/
-cd /etc/nginx/sites-enabled
-sudo rm *
-sudo ln -sf ../sites-available/* .
-sudo systemctl restart nginx.service
